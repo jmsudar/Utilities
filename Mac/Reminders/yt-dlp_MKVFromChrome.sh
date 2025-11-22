bcaf38483574e4e2a5679bb84cf7ab094d22d647
Joseph Sudar <jmsudar@gmail.com>
2025-11-22 15:36:14 -0800

    Added download with metadata script

--- Mac/Reminders/yt-dlp_MKVFromChrome.sh
+++ Mac/Reminders/yt-dlp_MKVFromChrome.sh
@@ -1,2 +1,2 @@
-echo 'Copied to clipboard: yt-dlp -f "bv*[vcodec^=avc]+ba[ext=m4a]/b[ext=mp4]/b" --merge-output-format mkv --cookies-from-browser chrome ""'
-echo 'yt-dlp -f "bv*[vcodec^=avc]+ba[ext=m4a]/b[ext=mp4]/b" --merge-output-format mkv --cookies-from-browser chrome ""' | pbcopy
+echo 'Copied to clipboard: yt-dlp -f "bv*[vcodec^=avc]+ba[ext=m4a]/b[ext=mp4]/b" --merge-output-format mkv -o "%(title)s.%(ext)s" --cookies-from-browser chrome ""'
+echo 'yt-dlp -f "bv*[vcodec^=avc]+ba[ext=m4a]/b[ext=mp4]/b" --merge-output-format mkv -o "%(title)s.%(ext)s" --cookies-from-browser chrome ""' | pbcopy
--- Mac/Reminders/yt-dlp_MP3FromChrome.sh
+++ Mac/Reminders/yt-dlp_MP3FromChrome.sh
@@ -1,2 +1,2 @@
-echo 'Copied to clipboard: yt-dlp -x --audio-format mp3 --cookies-from-browser chrome ""'
-echo 'yt-dlp -x --audio-format mp3 --cookies-from-browser chrome ""' | pbcopy
+echo 'Copied to clipboard: yt-dlp -x --audio-format mp3 --cookies-from-browser chrome -o "%(title)s.%(ext)s" ""'
+echo 'yt-dlp -x --audio-format mp3 --cookies-from-browser chrome -o "%(title)s.%(ext)s" ""' | pbcopy
