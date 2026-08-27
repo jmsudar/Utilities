603633c300ef61f554774a2f92546dbcd3b2d687
Joseph Sudar <jmsudar@gmail.com>
2026-08-27 09:26:01 -0700

    added cookie method

--- Mac/Reminders/wget_downloadFromTarget.sh
+++ Mac/Reminders/wget_downloadFromTarget.sh
@@ -8,5 +8,15 @@ fi
 
 identifier=$1
 url="https://archive.org/services/loans/loan/?action=media_url&identifier=${identifier}&format=pdf&redirect=1"
+cookie_file=$(mktemp -t archive_cookies)
+trap 'rm -f -- "$cookie_file"' EXIT
 
-wget -O "${identifier}.acsm" "$url"
+yt-dlp --cookies-from-browser chrome \
+       --cookies "$cookie_file" \
+       --skip-download \
+       "https://archive.org" 2>/dev/null || true
+
+wget --load-cookies "$cookie_file" \
+     --user-agent="Mozilla/5.0" \
+     -O "${identifier}.acsm" \
+     "$url"
